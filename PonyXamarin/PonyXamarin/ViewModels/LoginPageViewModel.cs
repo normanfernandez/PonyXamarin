b49bd534f86be18a7ef9339d5a4b38c951d0d671
Norman <el.norman01@gmail.com>
2017-10-08 23:24:11 -0400

    Credentials set to blank to avoid null

--- PonyXamarin/PonyXamarin/ViewModels/LoginPageViewModel.cs
+++ PonyXamarin/PonyXamarin/ViewModels/LoginPageViewModel.cs
@@ -19,6 +19,8 @@ namespace PonyXamarin.ViewModels
         {
             NavigationService = navigationService;
             PageDialogService = pageDialogService;
+            Username = "";
+            Password = "";
             Login = new DelegateCommand(ValidateLogin);
         }
 
